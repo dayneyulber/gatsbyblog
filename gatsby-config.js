e956de715cecc2658dbd6dec436ec098bc5c1ea6
dayne beronque <dayneyulber@gmail.com>
2020-01-25 11:49:04 -0600

    minor site detail changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Dayne Beronque`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Dayne Beronque`,
+    createdAt: 2020,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
